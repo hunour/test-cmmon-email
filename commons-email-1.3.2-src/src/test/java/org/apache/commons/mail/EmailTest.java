af4c540af1333988e376c1425076f45384855d8f
Hussein Nour Eddine <hunour@umich.edu>
2020-11-19 23:23:12 -0500

    Adding test function to test addBcc(String... emails), the test method name is testAddBcc()

--- commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,10 +1,16 @@
 package org.apache.commons.mail;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class EmailTest {
 	private EmailConcrete email;
+	
+	private static String[] TEST_EMAILS = {"ab@bc.com", "a.b@c.org", "abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
+	private static String[] BAD_TEST_EMAILS = {};
 	
 	@Before
 	public void setUpEmailTest() throws Exception{
@@ -13,4 +19,12 @@ public class EmailTest {
 	
 	@After
 	public void tearDownEmailTest() throws Exception{}
+	
+	//testing addBcc(String... emails) with good input
+		@Test
+		public void testAddBcc() throws Exception{
+			email.addBcc(TEST_EMAILS);
+			assertEquals(3, email.getBccAddresses().size());	
+		}
+	
 }
