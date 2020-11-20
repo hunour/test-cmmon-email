39a9d18b2cdd03adb7330adcad92b5a50657ea23
Hussein Nour Eddine <hunour@umich.edu>
2020-11-19 23:19:55 -0500

    Creating EmailTest.java class and adding the setup function to it, the method name is setUpEmailTest()

---
+++ commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,12 @@
+package org.apache.commons.mail;
+
+import org.junit.Before;
+
+public class EmailTest {
+	private EmailConcrete email;
+	
+	@Before
+	public void setUpEmailTest() throws Exception{
+		email = new EmailConcrete();
+	}
+}
